c218555d0d0373ac58ca54b302ea5541edbbcd99
atsfuny <56181628+atsfuny@users.noreply.github.com>
2022-06-16 12:38:07 +0430

    Update Home.js

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -42,10 +42,10 @@ const Home = ({ marketplace, nft }) => {
   useEffect(() => {
     loadMarketplaceItems()
   }, [])
-  if (loading) return (
+  /*if (loading) return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Loading...</h2>
-    </main>
+    </main>*/
   )
   return (
     <div className="flex justify-center">
